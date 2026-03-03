816e5e10a108a712492136545b3daa5b4f493383
jared0565 <jared0565@gmail.com>
2026-03-03 12:25:55 +0000

    Add basic vercel.json routing for static site

--- cleanup.py
+++
@@ -1,9 +0,0 @@
-import glob
-for f in glob.glob('*.html'):
-    with open(f, 'r', encoding='utf-8') as file:
-        content = file.read()
-    new_content = content.replace(' style="margin-top: 2rem; display: block;"', '')
-    if content != new_content:
-        with open(f, 'w', encoding='utf-8') as file:
-            file.write(new_content)
-        print(f"Cleaned {f}")
