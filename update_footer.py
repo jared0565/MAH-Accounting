01d78db4c7caa32c78e8f998ce83fbd3ba4f3495
jared0565 <jared0565@gmail.com>
2026-03-03 05:21:30 +0000

    services update

--- update_footer.py
+++
@@ -1,9 +0,0 @@
-import glob, re
-for f in glob.glob('*.html'):
-    with open(f, 'r', encoding='utf-8') as file:
-        content = file.read()
-    new_content = re.sub(r'<footer class="footer".*?</footer>', '<div id="global-footer"></div>', content, flags=re.DOTALL)
-    if content != new_content:
-        with open(f, 'w', encoding='utf-8') as file:
-            file.write(new_content)
-        print(f"Updated {f}")
